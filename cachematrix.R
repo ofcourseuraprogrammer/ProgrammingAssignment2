48a599ddd1339c80da6796f6abe21207cc1749bb
ofcourseuraprogrammer <handgrenadeofantioch@gmail.com>
2015-10-23 09:19:48 -0400

    Implemented function
    Functions implemented , with some effort to handle errors appropriately.

--- cachematrix.R
+++ cachematrix.R
@@ -6,22 +6,31 @@
 makeCacheMatrix <- function(x = matrix()) {
 
   m <- NULL
-  set 
+  set <- function(y) {
+      x <<- y
+      m <<- NULL
+  }
   
-  get
-  setcache
-  getcache
-  
-  
+  get <- function() x
+  setcache <- function(solve) m <<- solve
+  getcache <- function() m
+  list(set = set, get = get, 
+       setcache = setcache, getcache = getcache)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-
-  
+  m <- x$getcache()
+  if(!is.null(m)) {
+    message("getting the cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- tryCatch( solve(data), warning = function(w){print("WARNING")}, 
+                 error = function(e) {print("Singularity")})
+  x$setcache(m)
   m
   
 }
